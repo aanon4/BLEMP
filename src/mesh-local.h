d1e50b04e2c5029f7286eacf5c04d961e8dd6083
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2015-08-17 22:49:55 -0700

    Local mesh overrides

---
+++ src/mesh-local.h
@@ -0,0 +1,11 @@
+/*
+ * mesh-local.h
+ *
+ *  Created on: Aug 17, 2015
+ *      Author: tim
+ */
+
+#ifndef MESH_LOCAL_H_
+#define MESH_LOCAL_H_
+
+#endif /* MESH_LOCAL_H_ */
---
+++ xcode/Simulator/mesh-local.h
@@ -0,0 +1,13 @@
+//
+//  mesh-local.h
+//  Mesh
+//
+//  Created by tim on 8/17/15.
+//  Copyright (c) 2015 tim. All rights reserved.
+//
+
+#ifndef Mesh_mesh_local_h
+#define Mesh_mesh_local_h
+
+
+#endif
